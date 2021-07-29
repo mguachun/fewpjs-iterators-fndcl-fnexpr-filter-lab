78d5e2301ece1004165ec90fa260edb1e8962501
Melissa Guachun <1212malka@gmail.com>
2021-07-29 12:02:35 -0400

    commit tree 24418e92d130dbeea753c9614c3f102dd5e82af7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(driver, s) {
+    return driver.filter( possibleMatch =>
+        possibleMatch.toLowerCase() === s.toLowerCase()
+    )
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+function matchName(driver, soughtName) {
+    return driver.filter( record => record.name === soughtName)
+}
+
